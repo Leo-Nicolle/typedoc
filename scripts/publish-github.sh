70639ea9ac299f12da7b237c87a6cc83e831b101
Leo Nicolle <leo@linkurio.us>
2023-01-20 15:54:38 +0100

    better script

--- scripts/publish-github.sh
+++ scripts/publish-github.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
-npm pack
+# npm pack
 file=$(find . -name "typedoc*.tgz")
 echo "found $file"
 tar zxf $file
 cp -r package/* /tmp/typedoc
+cp -r . /tmp/save-typedoc
 git checkout dist
 rm -rf ./* .eslintrc .config .vscode .gitignore .editorconfig .npmrc
 cp -r /tmp/typedoc/* .
 rm -rf package
+git add .
+git commit -m "release"
+git push
+git checkout -
+cp -r /tmp/save-typedoc/* .
